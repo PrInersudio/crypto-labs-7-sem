9c0692dfe3d6525ed7a9c7a924ba00ff6cd48947
Семин Владислав <sminvlad@rambler.ru>
2023-11-18 21:18:14 +0300

    Поправил ошибку в комментах

--- find_set_bits.c
+++ find_set_bits.c
@@ -1,13 +1,13 @@
 #include "find_set_bits.h"
 
-// вычисляет позизию старшего установленного бита
+// вычисляет позицию старшего установленного бита
 unsigned last_set_bit(long unsigned n) {
     unsigned pos;
     asm volatile("bsr %0, %1": "=r"(pos) : "r"(n));
     return pos;
 }
 
-// вычисляет позизию младшего установленного бита
+// вычисляет позицию младшего установленного бита
 unsigned first_set_bit(long unsigned n) {
     unsigned pos;
     asm volatile("bsf %0, %1": "=r"(pos) : "r"(n));
--- find_set_bits.h
+++ find_set_bits.h
@@ -1,9 +1,9 @@
 #ifndef __FIND_SET_BITS__
 #define __FIND_SET_BITS__
 
-// вычисляет позизию старшего установленного бита
+// вычисляет позицию старшего установленного бита
 unsigned last_set_bit(long unsigned n);
-// вычисляет позизию младшего установленного бита
+// вычисляет позицию младшего установленного бита
 unsigned first_set_bit(long unsigned n);
 
 #endif
